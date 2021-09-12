4158904deeb0a7c8bade6199fb47da4a0bd1d649
acyang97 <acyang97@gmail.com>
2021-09-12 22:54:38 +0800

    Changed back baseURL

--- src/utils/server.js
+++ src/utils/server.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 // custom instance
 const server = axios.create({
-  baseURL: "http://localhost:5000"
-  // baseURL: "https://cs3216-timeline.herokuapp.com"
+  // baseURL: "http://localhost:5000"
+  baseURL: "https://cs3216-timeline.herokuapp.com"
 })
 
 export default server;
