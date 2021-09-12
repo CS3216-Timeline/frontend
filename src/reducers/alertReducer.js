eba4df408c742e95bcfb0d296000593daef45f8d
acyang97 <acyang97@gmail.com>
2021-09-13 03:04:31 +0800

    Removed old alertReducer code

--- src/reducers/alertReducer.js
+++ src/reducers/alertReducer.js
@@ -1,35 +1,3 @@
-// import {
-//   SET_ALERT,
-//   REMOVE_ALERT,
-// } from "../action-types/alert";
-
-// const initialState = {
-//   message: null,
-// };
-
-// const alertReducer = (state = initialState, action) => {
-//   const {
-//     type,
-//     payload
-//   } = action;
-//   switch (type) {
-//     case SET_ALERT:
-//       return {
-//         ...state,
-//         message: payload.message,
-//       };
-//     case REMOVE_ALERT:
-//       return {
-//         ...state,
-//         message: null,
-//       };
-//     default:
-//       return state;
-//   }
-// };
-
-// export default alertReducer;
-
 import {
   SET_ALERT,
   REMOVE_ALERT,
