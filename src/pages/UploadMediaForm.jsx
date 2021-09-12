15c24e3beaa20901f5714f93490f8f7e18cdc316
marcus2k <60178730+marcus2k@users.noreply.github.com>
2021-09-12 20:28:07 +0800

    Move firebase logic into services

--- src/pages/UploadMediaForm.jsx
+++ src/pages/UploadMediaForm.jsx
@@ -1,8 +1,6 @@
-import { Box, Button, Input } from "@material-ui/core";
-import { memo, useState } from "react"
-import { Redirect } from "react-router";
-import { storage } from "../services/firebase"
-import { postMediaUrl } from "../services/media";
+import { Box, Button } from "@material-ui/core";
+import { useState } from "react"
+import { uploadFile } from "../services/firebase"
 
 const DEFAULT_PHOTO = "https://firebasestorage.googleapis.com/v0/b/cs3216-timeline.appspot.com/o/user-media%2Fnone.jpg?alt=media&token=658f9a1d-9f16-4ace-ad56-c9a7c0e3132e"
 
@@ -16,7 +14,7 @@ const UploadMediaForm = props => {
   const { memory_id } = props;
   const [file, setFile] = useState(null);
   const [url, setUrl] = useState(null)
-  const [uploadProgress, setUploadProgress] = useState(0);
+  const [uploadProgress, setUploadProgress] = useState(null);
 
   const handleChange = e => {
     const newFile = e.target.files[0];
@@ -32,34 +30,26 @@ const UploadMediaForm = props => {
 
   const fileIsEmpty = file === null
 
+  const progressHandler = (percentage) => {
+    setUploadProgress(percentage);
+  }
+
+  const errorHandler = () => {
+    // TODO: show an alert
+    console.log("set up error alert here!");
+    setUploadProgress(null);
+    setFile(null);
+    setUrl(null);
+  }
+
+  const successHandler = (url) => {
+    setUrl(url);
+    setUploadProgress(null);
+    setFile(null);
+  }
+  
   const handleUpload = () => {
-    const uploadTask = storage.ref(`user-media/${memory_id}/${file.name}`).put(file)
-    uploadTask.on(
-      "state_changed",
-      snapshot => {
-        const progress = Math.round(
-          (snapshot.bytesTransferred / snapshot.totalBytes) * 100
-        )
-        setUploadProgress(progress)
-      }, 
-      error => {
-        // TODO: show an alert
-        console.log("set up error alert here!");
-        setUploadProgress(0);
-        setFile(null);
-        setUrl(null);
-      },
-      () => {
-        storage
-          .ref("user-media")
-          .child(`${memory_id}/${file.name}`)
-          .getDownloadURL()
-          .then(url => {
-            postMediaUrl(url, memory_id);
-            setUrl(url);
-          });
-      }
-    )
+    uploadFile(file, file.name, memory_id, progressHandler, errorHandler, successHandler);
   }
 
   return (
@@ -67,10 +57,10 @@ const UploadMediaForm = props => {
       {/* Just to view info */}
       <Box display="flex" flexDirection="column" style={{textAlign: "center"}}>
         <h3>Upload a photo</h3>
-        <img src={url || DEFAULT_PHOTO} alt="memory photo preview"/>
+        <img src={url || DEFAULT_PHOTO} alt="memory preview"/>
         <h5>{file ? file.name : "No file selected"}</h5>
         <input ref={file} style={inputStyle()} type="file" accept="image/*" onChange={handleChange} />
-        upload progress: {uploadProgress}%
+        {uploadProgress != null && `upload progress: ${uploadProgress}%`}
         <Button onClick={handleUpload} disabled={fileIsEmpty}>Save</Button>
       </Box>
     </>
