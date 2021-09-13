72f6ab05628ef0f112ff3413db59880e787e995f
acyang97 <acyang97@gmail.com>
2021-09-14 00:09:10 +0800

    Changed CircularProgress to custom Loading component

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { Redirect, Route } from "react-router-dom";
 import { useSelector } from "react-redux";
-import CircularProgress from "@material-ui/core/CircularProgress";
 import TopNavBar from "./layout/TopNavBar";
 import BottomNavBar from "./layout/BottomNavBar";
+import Loading from "./Loading";
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
   const auth = useSelector((state) => state.auth);
@@ -13,8 +13,7 @@ const PrivateRoute = ({ component: Component, ...rest }) => {
       {...rest}
       render={(props) =>
         auth.loading ? (
-          // TODO: Make the circular progress nicer
-          <CircularProgress />
+          <Loading />
         ) : auth.isAuthenticated ? (
           <>
             <TopNavBar />
