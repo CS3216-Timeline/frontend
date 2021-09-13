182ac936e85c559c47f7272afc13094e3164a19e
acyang97 <acyang97@gmail.com>
2021-09-13 23:55:17 +0800

    Updated loading screen

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -2,13 +2,25 @@ import React from "react";
 import { ClipLoader } from "react-spinners";
 import { makeStyles } from "@material-ui/core";
 import { COLORS } from "../utils/colors";
+import AppLogo from "./layout/AppLogo";
 
 const useStyles = makeStyles(() => ({
+  // loaderContainer: {
+  //   position: "absolute",
+  //   left: "50%",
+  //   top: "50%",
+  //   transform: "translate(-50%, -50%)",
+  // },
+  // items: {
+  //   display: "flex",
+  //   flexDirection: "column",
+  // },
   loaderContainer: {
-    position: "absolute",
-    left: "50%",
-    top: "50%",
-    transform: "translate(-50%, -50%)",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    height: `${window.innerHeight}px`,
   },
 }));
 
@@ -16,7 +28,8 @@ const Loading = () => {
   const classes = useStyles();
   return (
     <div className={classes.loaderContainer}>
-      <ClipLoader color={COLORS.PRIMARY_PURPLE} loading={true} size={30} />
+      <AppLogo />
+      <ClipLoader color={COLORS.PRIMARY_PURPLE} loading={true} size={50} />
     </div>
   );
 };
