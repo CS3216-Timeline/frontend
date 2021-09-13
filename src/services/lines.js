e95635fcb4b6a8c37afb185760e1d9e62aa15c97
marcus2k <60178730+marcus2k@users.noreply.github.com>
2021-09-14 02:58:02 +0800

    Separate memory services

--- src/services/lines.js
+++ src/services/lines.js
@@ -11,20 +11,6 @@ const mockLineData = {
   color: "blue",
 }
 
-const mockMemoryData = {
-  memory_id: 4,
-  date: "19 May 2021",
-  title: "Mok Family Gathering ",
-  description: "This is a mock card description. This is a mock card description. This is a mock card description. This is a mock card description. This is a mock card description.",
-  media: {
-    type: "IMAGE",
-    source: {
-      url: "https://www.whiteroomstudio.com.sg/wordpress/wp-content/uploads/2020/05/Big-Family-Photoshoot-Singapore_003.jpg"
-    }
-  }
-}
-
-
 export const getAllLinesByUserIdOrderByMostRecentMemory = async () => {
   const res = await server.get('/api/lines');
   let lines = res.data.lines;
@@ -51,12 +37,4 @@ export const createNewLine = async (lineTitle, selectedColor) => {
 // returns line data
 export const getLineById = (id) => {
   return mockLineData
-}
-
-// returns memory data
-export const getMemoryById = (id) => {
-  return {
-    ...mockMemoryData,
-    memory_id: id
-  }
 }
---
+++ src/services/memories.js
@@ -0,0 +1,20 @@
+const mockMemoryData = {
+  memory_id: 4,
+  date: "19 May 2021",
+  title: "Mok Family Gathering ",
+  description: "This is a mock card description. This is a mock card description. This is a mock card description. This is a mock card description. This is a mock card description.",
+  media: {
+    type: "IMAGE",
+    source: {
+    url: "https://www.whiteroomstudio.com.sg/wordpress/wp-content/uploads/2020/05/Big-Family-Photoshoot-Singapore_003.jpg"
+    }
+  }
+}
+
+// returns memory data
+export const getMemoryById = (id) => {
+  return {
+    ...mockMemoryData,
+    memory_id: id
+  }
+}
