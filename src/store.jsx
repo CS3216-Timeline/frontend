79c0d9ddd7fde29b976ba1d25a58d62786b2477d
acyang97 <acyang97@gmail.com>
2021-09-19 22:18:01 +0800

    Removed unused imports

--- src/store.jsx
+++ src/store.jsx
@@ -2,9 +2,6 @@ import { createStore, applyMiddleware, compose } from "redux";
 import { persistStore } from "redux-persist";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
-import { offline } from "redux-offline";
-import { createOffline } from "@redux-offline/redux-offline";
-import offlineConfig from "@redux-offline/redux-offline/lib/defaults";
 
 const initialState = {};
 
