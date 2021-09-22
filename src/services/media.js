7a1833f33c253d8f6cfd4c2e71bd92a129741a3e
marcus2k <60178730+marcus2k@users.noreply.github.com>
2021-09-22 23:24:43 +0800

    Add async, add logs to media services

--- src/services/media.js
+++ src/services/media.js
@@ -1,9 +1,16 @@
 import server from "../utils/server";
 
+// Use this function to convert blobURL to file
+const blobToFile = (blob, fileName="default-name", type="image/png") => {
+  const file = new File([blob], fileName, { type });
+  return file
+}
+
 // TODO: still draft
-export const postMediaUrl = (media, memoryId) => {
+export const createNewMedia = async (media, memoryId) => {
   const body = new FormData(); 
   body.append("memoryId", memoryId);
+  body.append("position", media.position);
 
   const loadImageFile = async (url, idx) => {
     const filename = `im-${idx}`;
@@ -19,12 +26,12 @@ export const postMediaUrl = (media, memoryId) => {
   for (const [key, value] of body.entries()) {
     console.log(`FormData[${key}]: ${value}`);
   }
-  const res = await server.post(`memories`, body);
+  const res = await server.post("media", body);
   console.log(`done POST media/${res.data.media.mediaId}...`);
-  return convertCoordinatesToFloat(res.data.media);
+  return res.data.media;
 }
 
-export const updateMediaPositions = (memoryId, mediaUrls) => {
+export const updateMediaPositions = async (memoryId, mediaUrls) => {
   const updates = mediaUrls.map(media => {
     const { mediaId, position } = media;
     return { mediaId, position };
@@ -36,8 +43,8 @@ export const updateMediaPositions = (memoryId, mediaUrls) => {
   console.log(`sending POST media/updates...`)
   console.log(body);
   const res = await server.post(`media`);
-  console.log(`done DELETE media/updates...`, body);
-  return convertCoordinatesToFloat(res.data.media);
+  console.log(`done POST media/updates...`, body);
+  return res.data.media;
 }
 
 export const getMediaUrl = (mediaId) => {
@@ -45,9 +52,9 @@ export const getMediaUrl = (mediaId) => {
     console.log("fetching media url for", mediaId)
 }
 
-export const deleteMediaById = (mediaId) => {
+export const deleteMediaById = async (mediaId) => {
   console.log(`sending DELETE media/${mediaId}...`)
   const res = await server.delete(`media/${mediaId}`);
   console.log(`done DELETE media/${mediaId}...`);
-  return convertCoordinatesToFloat(res.data.media);
+  return res.data.media;
 }
