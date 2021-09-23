2215211af51849e73edfaf7d30f0752386eb7d1f
acyang97 <acyang97@gmail.com>
2021-09-23 10:58:55 +0800

    Increase loading to 2 second

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,7 @@ const App = () => {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    setTimeout(() => setLoading(false), 1000);
+    setTimeout(() => setLoading(false), 2000);
   }, []);
 
   if (loading) {
