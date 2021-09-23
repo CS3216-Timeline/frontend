d62fc503e679c0cfca94c5ee059a0ff0d0691c29
acyang97 <acyang97@gmail.com>
2021-09-23 16:28:18 +0800

    revert

--- src/utils/server.js
+++ src/utils/server.js
@@ -4,8 +4,7 @@ const SERVER_BASE_URL = process.env.REACT_APP_SERVER_BASE_URL;
 
 // custom instance
 const server = axios.create({
-  baseURL: 'http://localhost:5000/api'
-  // baseURL: SERVER_BASE_URL,
+  baseURL: SERVER_BASE_URL,
 });
 
 export default server;
