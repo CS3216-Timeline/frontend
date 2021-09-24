b9bff55826ca6fd8a59db9e990bc5c8700f1eee8
marcus2k <60178730+marcus2k@users.noreply.github.com>
2021-09-24 18:21:53 +0800

    Update endpoints for users delete and patch

--- src/actions/auth.jsx
+++ src/actions/auth.jsx
@@ -152,7 +152,7 @@ export const logout = () => async (dispatch) => {
 
 export const deleteUserAccount = () => async (dispatch) => {
   try {
-    await server.delete("/users/delete");
+    await server.delete("/users");
     dispatch(logout());
     dispatch(setAlert("Account successfully deleted", "success"));
     logEvent(googleAnalytics, "user_deleted_account");
@@ -166,7 +166,7 @@ export const userChangeName = (newName) => async (dispatch) => {
     const body = {
       name: newName.trim(),
     };
-    const res = await server.patch("/users/profile", body);
+    const res = await server.patch("/users", body);
     const updatedUser = res.data.user;
     dispatch({
       type: CHANGE_NAME,
